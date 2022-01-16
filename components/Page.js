ee744422d3f7fec035041e730e386063d214d0ed
Alejandro Oviedo <alexoviedo999@gmail.com>
2022-01-16 13:04:42 -0600

    styled components and environment updates

--- components/Page.js
+++ components/Page.js
@@ -1,12 +1,54 @@
+import PropTypes from 'prop-types';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
 
-/* eslint-disable react/prop-types */
+const GlobalStyles = createGlobalStyle`
+  html {
+    --blue: #3f00ff;
+    --black: #393939;
+    --grey: #3A3A3A;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey);
+    --offWhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height:2;
+  }
+  a {
+    text-decoration: none;
+    color: var(---black);
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+  button {
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+  }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
 export default function Page({ children }) {
   return (
     <div>
-      <Header>The Header</Header>
-      <h2>I am the page</h2>
-      {children}
+      <GlobalStyles />
+      <Header />
+      <InnerStyles>{children}</InnerStyles>
     </div>
   );
 }
